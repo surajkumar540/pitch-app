5cd045f50a43d0669064e20d290350b15acdcaba
suraj <surajgautam56878@gmail.com>
2024-11-24 18:18:02 +0530

    feature:github auth flow fixed

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -14,7 +14,7 @@ const Navbar = async () => {
         <div className="flex items-center gap-5 text-black">
           {session && session?.user ? (
             <>
-              <Link href={"/'startup/create"}>
+              <Link href={"/startup/create"}>
                 <span>Create</span>
               </Link>
 
